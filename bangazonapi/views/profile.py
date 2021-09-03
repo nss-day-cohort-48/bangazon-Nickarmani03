40255d109420b9ffc2ed15b7051902ce5e79ba41
Nickarmani03 <nickarmani03@gmail.com>
2021-09-03 09:17:54 -0500

    cant remove item from cart

--- bangazonapi/views/profile.py
+++ bangazonapi/views/profile.py
@@ -185,8 +185,7 @@ class Profile(ViewSet):
 
                 cart = {}
                 
-                cart["order"] = OrderSerializer(open_order, many=False, context={'request': request}).data
-                
+                cart["order"] = OrderSerializer(open_order, many=False, context={'request': request}).data                
                 cart["order"]["size"] = len(lineitems.data)
 
             except Order.DoesNotExist as ex:
